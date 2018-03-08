1a546e088e8bccd5142dedf0564e607846cac1f7
Andreas Skarmutsos Lindh <andreas@superblock.se>
2018-03-08 14:59:28 +0100

    Add support for access keys from UI

--- src/main/java/io/jenkins/plugins/kinesislogs/KinesisClientHelper.java
+++ src/main/java/io/jenkins/plugins/kinesislogs/KinesisClientHelper.java
@@ -1,14 +1,38 @@
 package io.jenkins.plugins.kinesislogs;
 
-import com.amazonaws.regions.Region;
-import com.amazonaws.services.kinesis.*;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.services.kinesis.AmazonKinesisAsync;
+import com.amazonaws.services.kinesis.AmazonKinesisAsyncClientBuilder;
+import com.google.common.base.Strings;
 
 import java.util.logging.Logger;
 
 public class KinesisClientHelper {
     private static final Logger LOGGER = Logger.getLogger(KinesisClientHelper.class.getName());
 
-    public AmazonKinesisAsync createClient() {
-        return AmazonKinesisAsyncClientBuilder.defaultClient();
+    public AmazonKinesisAsync getAwsKinesis(final KinesisConfiguration config) {
+        // If we find access keys, construct our own client
+        if (!Strings.isNullOrEmpty(config.getAwsAccessKeyId()) && (!Strings.isNullOrEmpty(config.getAwsSecretKey()))) {
+            AWSCredentials credentials = new AWSCredentials() {
+                @Override
+                public String getAWSAccessKeyId() {
+                    return config.getAwsAccessKeyId();
+                }
+
+                @Override
+                public String getAWSSecretKey() {
+                    return config.getAwsSecretKey();
+                }
+            };
+
+            return AmazonKinesisAsyncClientBuilder.
+                    standard().
+                    withRegion(config.getAwsRegion()).
+                    withCredentials(new AWSStaticCredentialsProvider(credentials)).
+                    build();
+        }
+        // Otherwise, work through the default AWS SDK credentials chain
+        return AmazonKinesisAsyncClientBuilder.standard().build();
     }
 }
--- src/main/java/io/jenkins/plugins/kinesislogs/KinesisWriter.java
+++ src/main/java/io/jenkins/plugins/kinesislogs/KinesisWriter.java
@@ -1,17 +1,21 @@
 package io.jenkins.plugins.kinesislogs;
 
 import com.amazonaws.AmazonClientException;
+import com.amazonaws.handlers.AsyncHandler;
 import com.amazonaws.services.kinesis.AmazonKinesisAsync;
 import com.amazonaws.services.kinesis.model.PutRecordRequest;
+import com.amazonaws.services.kinesis.model.PutRecordResult;
 import hudson.model.Run;
 import hudson.model.TaskListener;
-import jenkins.model.Jenkins;
 import org.apache.commons.lang.StringUtils;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.logging.Logger;
 
 public class KinesisWriter {
@@ -21,6 +25,7 @@ public class KinesisWriter {
     private final Run<?, ?> build;
     private final TaskListener listener;
     private Charset charset;
+    private String partitionKey;
 
     private static final Logger LOGGER = Logger.getLogger(KinesisWriter.class.getName());
 
@@ -29,9 +34,27 @@ public class KinesisWriter {
         this.build = run;
         this.listener = listener;
         this.charset = charset;
-        this.kc = new KinesisClientHelper().createClient();
+        this.kc = new KinesisClientHelper().getAwsKinesis(KinesisConfiguration.get());
         this.streamName = KinesisConfiguration.get().getStreamName();
-        LOGGER.info("streamName: " + this.streamName);
+
+        /**
+         * Use the MD5 sum of the JobName string as partitionKey, so that we don't
+         * exceed the maximum char len of the Kinesis API
+         */
+        byte[] jobNameBytes = new byte[0];
+        try {
+            jobNameBytes = run.getParent().getName().getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        byte[] digest = md.digest(jobNameBytes);
+        this.partitionKey = digest.toString();
     }
 
     public Charset getCharset() {
@@ -40,23 +63,33 @@ public class KinesisWriter {
 
     public void write(String line) {
         if (StringUtils.isNotEmpty(line)) {
-//            LOGGER.info("Inside write method, writing");
             KinesisLogEvent event = new KinesisLogEvent(this.build, line);
             byte[] bytes = event.toJsonAsBytes();
             if (bytes == null) {
                 LOGGER.warning("Could not get JSON bytes for event");
+                return;
             }
 
             PutRecordRequest prr = new PutRecordRequest();
             prr.setStreamName(this.streamName);
-            prr.setPartitionKey(event.getJobName());
+            prr.setPartitionKey(this.partitionKey);
             prr.setData(ByteBuffer.wrap(bytes));
             try {
-                this.kc.putRecordAsync(prr);
+                this.kc.putRecordAsync(prr,
+                        new AsyncHandler<PutRecordRequest, PutRecordResult>() {
+                            @Override
+                            public void onError(Exception exception) {
+                                exception.printStackTrace();
+                            }
+
+                            @Override
+                            public void onSuccess(PutRecordRequest request, PutRecordResult putRecordResult) {
+                                // Do nothing.
+                            }
+                        });
             } catch (AmazonClientException ex) {
                 LOGGER.warning("Error sending record to Amazon Kinesis:" + ex.toString());
             }
-
         }
     }
 
@@ -73,8 +106,4 @@ public class KinesisWriter {
             ex.printStackTrace();
         }
     }
-
-
-
-
 }
